7fc24e406e625de9960bb88212528e777c1e2d7e
gen276 <113082932+gen276@users.noreply.github.com>
2022-12-04 10:28:55 +0900

    javascript課題1-2の提出
    javascript課題1-2の提出です

---
+++ JavaStudy/1-2/main.js
@@ -0,0 +1,20 @@
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
+
+// let name = "hoge";
+// //nameと書いたのに、hogeと出力されるはずです
+// console.log(name);
+
+// name = "fuga";
+// // console.log(name);
+
+// let name = "池田";
+// console.log(name + "くん");
+// let number = 10;
+// console.log(number * 3);
+
+// console.log(`${name}くんは${number}月生まれです`);
+// // ${}で文章の記入ができる
